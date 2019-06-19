aaef1051cd40762f1c2090937243033211f28030
Emily Hanks <han17007@byui.edu>
2019-06-18 21:55:55 -0600

    adding team activity 09

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+let url = require('url');
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.render('pages/team-activity09/form'))
+  .get('/math', (req, res) => { let result = calculate(req, res); res.render('pages/team-activity09/math', {result: result}); })
+  .get('/math_service', (req, res) => { let result = calculate(req, res); res.json({"result": result}); })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  function calculate(req, res) {
+    let query = url.parse(req.url, true).query;
+        
+    let lhs = parseFloat(query.lhs);
+    let operation = query.operation;
+    let rhs = parseFloat(query.rhs);
+    let result = 0;
+
+    if (operation == '+') {
+        result = lhs + rhs;
+    } else if (operation == '-') {
+        result = lhs - rhs;
+    } else if (operation == '*') {
+        result = lhs * rhs;
+    } else if (operation == '/') {
+        result = lhs / rhs;
+    } else {
+        result = 0;
+    }
+
+    console.log("Result " + result);
+    return result;
+    //res.render('pages/team-activity09/math', {result: result});
+  }
