a9baedeb26e0accb5b055db8fce489d91bfe1351
Emily Hanks <han17007@byui.edu>
2019-06-24 01:54:36 -0600

    attempting to get heroku up and running my server...

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ var express = require("express");
 var url = require("url");
 var app = express();
 
-app.get("/prove09", function(request, response) {
-    console.log("Received a request for /prove09");
+app.use(express.static("../../public/prove09"));
 
-    response.render("prove09/server.js");
+app.set("views", "../../views/prove09");
+app.set("view engine", "ejs");
+
+app.get("/getRate", function(request, response) {
+    console.log("Received a request for /getRate");
+    var query = url.parse(request.url, true).query;
+    var params = { weight: Number(query.weight), type: query.radioButtons };
+
+    response.render("getRate", params);
 })
+
+app.listen(5000, function() {
+	console.log("The server is up and listening on port 5000");
+});
