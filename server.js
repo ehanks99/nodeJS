38af1376f7685e9f59a048900832dfb1db5ec239
Emily Hanks <han17007@byui.edu>
2019-06-26 20:31:09 -0600

    trying again

--- server.js
+++ server.js
@@ -12,31 +12,4 @@ app.listen(5000, function() {
 
 /* teamActivity09 stuff */
 express()
-    .get('/team-activity09', (req, res) => res.render('pages/team-activity09/form'))
-    .get('/team-activity09/math', (req, res) => { let result = calculate(req, res); res.render('pages/team-activity09/math', {result: result}); })
-    .get('/team-activity09/math_service', (req, res) => { let result = calculate(req, res); res.json({"result": result}); });
-
-  function calculate(req, res) {
-    let query = url.parse(req.url, true).query;
-        
-    let lhs = parseFloat(query.lhs);
-    let operation = query.operation;
-    let rhs = parseFloat(query.rhs);
-    let result = 0;
-
-    if (operation == '+') {
-        result = lhs + rhs;
-    } else if (operation == '-') {
-        result = lhs - rhs;
-    } else if (operation == '*') {
-        result = lhs * rhs;
-    } else if (operation == '/') {
-        result = lhs / rhs;
-    } else {
-        result = 0;
-    }
-
-    console.log("Result " + result);
-    return result;
-    //res.render('pages/team-activity09/math', {result: result});
-  }
+    .get('/team-activity09', (req, res) => res.render('pages/team-activity09/form'));
