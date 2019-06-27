00690098d2d4946e9e69163b75e10aaf9ad618e1
Emily Hanks <han17007@byui.edu>
2019-06-26 20:27:24 -0600

    trying again

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.use(express.static("public"));
 app.set("views", "views");
 app.set("view engine", "ejs");
 
+app.listen(5000, function() {
+	console.log("The server is up and listening on port 5000");
+});
 
 /* teamActivity09 stuff */
 express()
@@ -37,20 +40,3 @@ express()
     return result;
     //res.render('pages/team-activity09/math', {result: result});
   }
-
-/* prove09 stuff */
-app.get("prove09/main.html", function(request, response) {
-    response.render("public/prove09/main.html");
-});
-
-app.get("prove09/getRate", function(request, response) {
-    console.log("Received a request for /prove09/getRate");
-    var query = url.parse(request.url, true).query;
-    var params = { weight: Number(query.weight), type: query.radioButtons };
-
-    response.render("pages/prove09/getRate", params);
-})
-
-app.listen(5000, function() {
-	console.log("The server is up and listening on port 5000");
-});
